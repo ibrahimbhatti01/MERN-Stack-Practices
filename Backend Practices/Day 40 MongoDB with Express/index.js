7a5ce2e2a96f4c19a6c197792e007a3a6e88640f
ibrahim <ib.8686299@gmail.com>
2024-10-12 12:01:45 +0500

    added '/chats' route

--- Backend Practices/Day 40 MongoDB with Express/index.js
+++ Backend Practices/Day 40 MongoDB with Express/index.js
@@ -6,6 +6,7 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then((res) => console.log("connection successful"))
@@ -17,6 +18,12 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("working finely");
+});
+
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("chats.ejs", {chats});
 });
 
 app.listen("8080", () => {
