95f154a17ecd7a73aec2f621ce34c2910b65606d
ibrahim <ib.8686299@gmail.com>
2024-10-12 10:46:54 +0500

    added chats model to database

--- Backend Practices/Day 40 MongoDB with Express/index.js
+++ Backend Practices/Day 40 MongoDB with Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -13,6 +14,18 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "Noman",
+  to: "ibrahim",
+  message: "Parh liya kr, khuda da nam e, ty interview dy dy, English sikh ly",
+  createdAt: new Date(),
+});
+
+chat1
+  .save()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("working finely");
