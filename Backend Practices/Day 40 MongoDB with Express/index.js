0139c127f0b28aba8ce74dbb97a636ab0963283c
ibrahim <ib.8686299@gmail.com>
2024-10-12 23:19:10 +0500

    added delete route, eith confirmation

--- Backend Practices/Day 40 MongoDB with Express/index.js
+++ Backend Practices/Day 40 MongoDB with Express/index.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then((res) => console.log("connection successful"))
@@ -31,18 +33,48 @@ app.get("/chats/new", (req, res) => {
 });
 
 app.post("/chats", (req, res) => {
-  let {from, to, message} = req.body;
+  let { from, to, message } = req.body;
   let newChat = new Chat({
     from: from,
     message: message,
     to: to,
-    createdAt: new Date()
+    createdAt: new Date(),
   });
   newChat
-  .save()
-  .then(res => {console.log("New message send !")})
-  .catch(err => console.log(err));
+    .save()
+    .then((res) => {
+      console.log("New message send !");
+    })
+    .catch((err) => console.log(err));
+  res.redirect("/chats");
+});
 
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+app.post("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { message: newMessage } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    {
+      message: newMessage,
+    },
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
+  console.log(updatedChat);
+  res.redirect("/chats");
+});
+
+app.delete("/chats/:id/delete", async (req, res) => {
+  let { id } = req.params;
+  let deleted = await Chat.findByIdAndDelete(id);
   res.redirect("/chats");
 });
 
