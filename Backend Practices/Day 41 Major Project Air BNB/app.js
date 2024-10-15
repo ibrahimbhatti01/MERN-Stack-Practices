02cc8ac822022783ccc492c24b0e4edf6a77a049
ibrahim <ib.8686299@gmail.com>
2024-10-15 12:45:24 +0500

    added edit and update listing route

--- Backend Practices/Day 41 Major Project Air BNB/app.js
+++ Backend Practices/Day 41 Major Project Air BNB/app.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -45,6 +47,21 @@ app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  console.log(req.body.listing);
+  res.redirect(`/show/${id}`);
 });
 
 app.listen("8080", () => {
