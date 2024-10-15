27b513d5a5287fdbd3758f1bf06859e6b91cdb8c
ibrahim <ib.8686299@gmail.com>
2024-10-15 16:15:06 +0500

    basic ejs-mate templates and styling setup

--- Backend Practices/Day 41 Major Project Air BNB/app.js
+++ Backend Practices/Day 41 Major Project Air BNB/app.js
@@ -4,11 +4,14 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -59,18 +62,18 @@ app.get("/listings/:id/edit", async (req, res) => {
 //Update Route
 app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   console.log(req.body.listing);
   res.redirect(`/show/${id}`);
 });
 
 //Delete Route
 app.delete("/listings/:id/delete", async (req, res) => {
-  let {id} = req.params;
+  let { id } = req.params;
   const deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing)
+  console.log(deletedListing);
   res.redirect("/listings");
-})
+});
 
 app.listen("8080", () => {
   console.log("Server is listening at port 8080");
