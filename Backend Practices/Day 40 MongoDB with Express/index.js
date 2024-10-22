de20bf1b2d4ea89dbfa7b8f7416e029a4db17510
ibrahim <ib.8686299@gmail.com>
2024-10-22 11:33:42 +0500

    some basic error handling

--- Backend Practices/Day 40 MongoDB with Express/index.js
+++ Backend Practices/Day 40 MongoDB with Express/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError.js")
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,23 +17,28 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+  await mongoose.connect("mongodb://127.0.0.1:27017/fakewhatsapp");
 }
 
+// root
 app.get("/", (req, res) => {
-  res.send("working finely");
+  res.send("working fine");
 });
 
+// index route
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
   res.render("chats.ejs", { chats });
 });
 
+// new route
 app.get("/chats/new", (req, res) => {
+  // throw new ExpressError(404, "page not found");
   res.render("new.ejs");
 });
 
-app.post("/chats", (req, res) => {
+// create route
+app.post("/chats", async (req, res) => {
   let { from, to, message } = req.body;
   let newChat = new Chat({
     from: from,
@@ -40,21 +46,29 @@ app.post("/chats", (req, res) => {
     to: to,
     createdAt: new Date(),
   });
-  newChat
-    .save()
-    .then((res) => {
-      console.log("New message send !");
-    })
-    .catch((err) => console.log(err));
+  await newChat.save();
   res.redirect("/chats");
 });
 
+// show route
+app.get("/chats/:id", async (req, res, next) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  if(!chat){
+    // throw new ExpressError(404, "page not found"); //in async funcyion express can't call next() on it's own.
+    next(new ExpressError(404, "page not found")); //So, we've to do in this type
+  }
+  res.render("show.ejs", { chat });
+});
+
+// edit route
 app.get("/chats/:id/edit", async (req, res) => {
   let { id } = req.params;
   let chat = await Chat.findById(id);
   res.render("edit.ejs", { chat });
 });
 
+// update route
 app.post("/chats/:id", async (req, res) => {
   let { id } = req.params;
   let { message: newMessage } = req.body;
@@ -72,11 +86,18 @@ app.post("/chats/:id", async (req, res) => {
   res.redirect("/chats");
 });
 
+// delete route
 app.delete("/chats/:id/delete", async (req, res) => {
   let { id } = req.params;
   let deleted = await Chat.findByIdAndDelete(id);
   res.redirect("/chats");
 });
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  let {status=500, message} = err;
+  res.status(status).send(message);
+})
 
 app.listen("8080", () => {
   console.log("server is listening on 8080");
