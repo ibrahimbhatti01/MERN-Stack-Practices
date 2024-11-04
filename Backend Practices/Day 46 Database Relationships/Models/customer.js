ae35fecc8ebc78ff9fd113e957b74bfed2066f44
ibrahim <ib.8686299@gmail.com>
2024-11-04 16:11:32 +0500

    One to many || Approach 2

---
+++ Backend Practices/Day 46 Database Relationships/Models/customer.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+main()
+    .then(() => console.log("connection successful"))
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+};
+
+const orderSchema = new Schema({
+        item: String,
+        Price: Number,
+    });
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [{
+        type: Schema.Types.ObjectId,
+        ref: "Order"
+    }]
+});
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+    let customer1 = new Customer({
+        name: "ibrahim",
+    })
+
+    let order1 = await Order.findOne({item: "Chips"});
+    let order2 = await Order.findOne({item: "Wrapsta"});
+
+    customer1.orders.push(order1);
+    customer1.orders.push(order2);
+
+    let result = await customer1.save();
+    console.log(result);
+}
+
+addCustomer()
+
+// const addOrder = async () => {
+//     let result = await Order.insertMany(
+//         [
+//             {
+//                 item: "Chips",
+//                 Price: 50
+//             },
+//             {
+//                 item: "Wrapsta",
+//                 Price: 170
+//             },
+//             {
+//                 item: "Peppricorn",
+//                 Price: 350
+//             }
+//         ]
+//     );
+//     console.log(result);
+// };
+
+// addOrder();
