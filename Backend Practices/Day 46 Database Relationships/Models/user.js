a1da2605a12835dce602c36644fc6ab4068caffd
ibrahim <ib.8686299@gmail.com>
2024-11-04 12:16:03 +0500

    Relationships in databases

--- Backend Practices/Day 46 Database Relationships/Models/user.js
+++ Backend Practices/Day 46 Database Relationships/Models/user.js
@@ -1,47 +1,41 @@
 const mongoose = require("mongoose");
-const {Schema} = require("mongoose");
+const { Schema } = require("mongoose");
 
 main()
-.then(() => console.log("Connection Successful"))
-.catch(err => console.log(err));
+    .then(() => console.log("connection successful"))
+    .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 };
 
-const userSchema = new Schema ({
-  username: String,
-  addresses: [
-    {
-      _id: false,
-    location: String,
-    country: String
-    }
-  ]
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+        _id: false,
+        location: String,
+        country: String
+        }
+    ]
 });
 
 const User = mongoose.model("User", userSchema);
 
 const addUser = async () => {
-  let user1 = new User({
-    username: "sherlokholmes",
-    addresses: [
-      {
-        location: "348-A, Street No.02, Sahiwal",
-        country: "Pakistan"
-      }
-    ]
-  })
+    let user1 = new User({
+        username: "ibrahim",
+        addresses: [
+            {
+                location: "347-B, Pasha street, Orangzeb Market, Sahiwal",
+                country: "Pakistan"
+            }
+        ]
+    });
 
-  user1.addresses.push(
-    {
-    location: "233-C, Street No.07, Sahiwal",
-    country: "Pakistan"
-    }
-  );
-
-  let result = await user1.save();
-  console.log(result);
+    user1.addresses.push({location: "569-K, Dumb street, Blind Market, Sahiwal", country: "Pakistan"})
+    let result = await user1.save();
+    console.log(result);
 };
 
 addUser();
