33ea96da1a5f390ed45234f4821b183ab6b134a4
Ibrahim <ib.8686299@gmail.com>
2025-09-30 11:47:32 +0500

    learnt array method 1. arr.forEach()

---
+++ Backend Practices/Day 21 Array adv methods & SAC/app.js
@@ -0,0 +1,48 @@
+// let arr = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+// let count = 0;
+// let sumF = (element) => {
+//     console.log(this);
+//     sum += element;
+// }
+
+// arr.forEach(sumF);
+
+// console.log(sum / arr.length);
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+// arr.forEach(function(element){sum+=element});
+
+// console.log(sum/arr.length);
+
+
+// let cart = [{
+//     name: "icebar",
+//     price: 100
+// },{
+//     name: "mouse",
+//     price: 500
+// },{
+//     name: "laptop",
+//     price: 5000
+// },{
+//     name: "pen",
+//     price: 400
+// }];
+
+// let total = 0;
+// let products = [];
+// cart.forEach((product) => {
+//     total += product.price;
+//     products.push(product.name);
+//     console.log(this);
+// })
+
+// console.log(total, "\n", products);
+
+
+
