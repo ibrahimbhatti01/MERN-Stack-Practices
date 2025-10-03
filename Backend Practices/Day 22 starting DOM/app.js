9bfb8a5ac9b49a4aed4546910a02151170118cbe
Ibrahim <ib.8686299@gmail.com>
2025-10-03 23:02:11 +0500

    done with problem sets day 22

--- Backend Practices/Day 22 starting DOM/app.js
+++ Backend Practices/Day 22 starting DOM/app.js
@@ -226,3 +226,50 @@
 
 
 
+
+// Qsl. Create a new input and button element on the page using JavaScript only. Set the
+
+// text of button to "Click me'
+
+let input = document.createElement("input");
+let button = document.createElement("button");
+
+let body  = document.querySelector("body");
+body.prepend(button);
+body.prepend(input);
+
+button.innerText = "Press me";
+
+
+// Qs2. Add following attributes to the element :
+
+// Change placeholder value of input to "username"
+// Change the id of button to "btn'
+
+input.placeholder = "username";
+button.id = "btn";
+
+
+// Qs3. Access the btn using the querySelector and button id. Change the button background
+// color to blue and text color to white.
+
+document.querySelector("#btn").style.background = "blue";
+document.querySelector("#btn").style.color = "white";
+
+
+// Qs4. Create an hl element on the page and set its text to "DOM Practice" underlined.
+// Change its color to purple.
+
+let h1 = document.createElement("h1");
+h1.innerText = "DOM Practice";
+body.prepend(h1);
+
+h1.classList.add("custom-title");
+
+
+// Qs5. Create a p tag on the page and set its text to "Apna College Delta Practice",
+// where Delta is bold.
+
+let pTag = document.createElement("p");
+pTag.innerHTML = `Apna Vision <b>Delta</b> Practice`;
+body.insertAdjacentElement("afterbegin", pTag);
