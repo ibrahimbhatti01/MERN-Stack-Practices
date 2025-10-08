c0364acbc2531187250d131b08f288f7bb7d21d2
Ibrahim <ib.8686299@gmail.com>
2025-10-08 21:12:43 +0500

    Done with Simon Says Game

--- Backend Practices/Day 24 Mini Project javaScriptToDo list, Simon Says Game/Simon Says Game/app.js
+++ Backend Practices/Day 24 Mini Project javaScriptToDo list, Simon Says Game/Simon Says Game/app.js
@@ -1,55 +1,41 @@
 let gameSeq = [];
 let userSeq = [];
-let buttons = [];
+let buttons = document.querySelectorAll(".box");
 
 let gameStarted = false;
 let level = 0;
 
 document.addEventListener("keypress", function(){
     if(!gameStarted){
+        gameStarted = true;
 
-        for(btn of document.querySelectorAll(".box")){
-            buttons.push(btn);
-        };
-
-        console.log("key pressed, game started");
+        document.querySelector(".title").innerText = `Simon Says Gam 3`;
 
         levelUpdate();
-        
-        gameStarted = true;
     }
 })
 
 // do simple button flash
-function buttonFlash(button){
-    button.style.backgroundColor = "white";
+function buttonFlash(button, delay = 0){
     setTimeout(() => {
-        //remove inline style, cs style will be applied back
-        button.style.backgroundColor = "";
-    }, 150);
+        button.style.backgroundColor = "white";
+        setTimeout(() => {
+            //remove inline style, cs style will be applied back
+            button.style.backgroundColor = "";
+        }, 200);
+    }, delay)
 }
 
 //generate new button flash to buildup sequence
-function genRandButtonFlash(){
+function genRandButtonFlash(delay = 0){
     let randomBtn = Math.floor(Math.random()*4);
-
-    ////it'll just work with using window.getComputedStyle("element");
-    // let btnColor = buttons[randomBtn].style.backgroundColor;
-    // console.log(btnColor);
-
-    //style inline, which has more specificity
-    buttons[randomBtn].style.backgroundColor = "white";
-    setTimeout(() => {
-        //remove inline style, cs style will be applied back
-        buttons[randomBtn].style.backgroundColor = "";
-    }, 150);
-
+    buttonFlash(buttons[randomBtn], delay);
     return buttons[randomBtn];
 }
 
 //compare and check both sequences
 function checkSeq(){
-    for(let i=0; i<=level; i++){
+    for(let i=0; i<userSeq.length; i++){
         if(!(gameSeq[i] === userSeq[i]))
             return false;
     }
@@ -58,42 +44,63 @@ function checkSeq(){
 
 //update the level
 function levelUpdate(){
-    level++;
-
-    for(btn of gameSeq){
-        buttonFlash(btn);
-    }
-
-    document.querySelector(".score-board").innerText = `Level ${level}`;
+    if(level++)
+        userSeq = [];
 
     gameSeq.push(genRandButtonFlash());
+    document.querySelector(".score-board").innerText = `Level ${level}`;
 
-    inputUserSeq();
-
-    // if(checkSeq()){
-    //    userSeq = [];
-    //    levelUpdate();}
-    // else
-    //    gameOver();
+    //Take and buildUp user sequence
+    for(userBtn of document.querySelectorAll(".box")){
+        userBtn.addEventListener("click", inputUserSeq);
+    }
 }
 
 function gameOver(){
-    document.querySelector(".score-board").innerText = `Game Over, Your Highest Score Was: ${level}`;
-    document.querySelector("*").style.color = "#B00020";
+    const highestScore = level-1;
+    console.log(highestScore);
+    document.querySelector(".score-board").innerHTML = `Game Over 😙, Your Score is: <b>${highestScore}</b> <br> Press any key to continue.`;
+    document.querySelector("body").style.backgroundColor = "#B00020";
+
+    if(highestScore < 4){
+        document.querySelector(".title").innerText = `Hmm, Keep it up 😁`;
+    }else if(highestScore < 7 && highestScore > 3){
+        document.querySelector(".title").innerText = `Ahh, Played well, Here's your Toy 🍭😁`;
+    }else {
+        document.querySelector(".title").innerText = `Dang, Genius is here 😃. You deserve 🍏`;
+    }
 
     setTimeout(() => {
-        document.querySelector("*").style.color = "";
-    }, 100);
+        document.querySelector("body").style.backgroundColor = "";
+    }, 150);
+
+    console.log(gameSeq);
+    console.log(userSeq);
+    gameSeq = [];
+    userSeq = [];
+
+    for(userBtn of document.querySelectorAll(".box")){
+        userBtn.removeEventListener("click", inputUserSeq);
+    }
+
+    gameStarted = false;
+    level = 0;
+
+    return;
 }
 
-function inputUserSeq(){
-    for(let i=1; i<=level; i++){
-        //Take and buildUp user sequence
-        for(userBtn of document.querySelectorAll(".box")){
-            userBtn.addEventListener("click", function(){
-                userSeq.push(this);
-                buttonFlash(this);
-            })
+function inputUserSeq(event){
+    if(userSeq.length <= gameSeq.length){
+        // console.log(this);
+        userSeq.push(event.target);
+        buttonFlash(event.target);
+        if(!checkSeq())
+            setTimeout(() => gameOver(), 200);
+        else if(userSeq.length === gameSeq.length){
+            for(userBtn of document.querySelectorAll(".box")){
+                userBtn.removeEventListener("click", inputUserSeq);
+            }
+            setTimeout(() => levelUpdate(), 500);
         }
     }
 }
