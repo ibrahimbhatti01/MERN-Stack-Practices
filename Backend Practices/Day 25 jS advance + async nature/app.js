83b11a73dd36ca7bd0eb4b1747b081d2bcec0aa8
Ibrahim <ib.8686299@gmail.com>
2025-10-09 17:20:07 +0500

    done with awareness to callback hell

--- Backend Practices/Day 25 jS advance + async nature/app.js
+++ Backend Practices/Day 25 jS advance + async nature/app.js
@@ -36,8 +36,48 @@
 
 
 
-// setTimeout(() => {
+// 
 //     console.log("Kaiy hoo? businessman");
 // }, 1000);
 
 // console.log("hello..");
+
+
+
+
+
+
+let h2 = document.querySelector(".salam");
+
+function changeColor(color, callback, delay = 1000){
+    setTimeout(() => {
+        h2.style.color = `${color}`;
+        if(callback)
+            callback();
+    }, delay)
+}
+
+// Call Back HELLL 🔥💀
+changeColor("#F48020", ()=>{
+    changeColor("#E60026", () => {
+        changeColor("#40E0D0", () => {
+            changeColor("#720e9e", ()=>{
+                changeColor("#FEBE10", () => {
+                    changeColor("#50C878", () => {
+                        changeColor("#1F75FE", () => {
+                            changeColor("#FFFF00", ()=>{
+                                changeColor("#4B5320", () => {
+                                    changeColor("#EE82EE")
+                                });
+                            });
+                        });
+                    });
+                });
+            })
+        });
+    });
+});
+
+
+
+
