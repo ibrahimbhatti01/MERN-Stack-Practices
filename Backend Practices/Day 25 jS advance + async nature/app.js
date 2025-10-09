0f2e3548ad552bc674bc8eeddbe128d8a04456a5
Ibrahim <ib.8686299@gmail.com>
2025-10-09 12:37:22 +0500

    done with javaScript Call Stack

---
+++ Backend Practices/Day 25 jS advance + async nature/app.js
@@ -0,0 +1,16 @@
+// function hello(){
+//     console.log("inside hello funx");
+//     console.log("hello");
+// }
+
+// function demo(){
+//     console.log("inside demo funx")
+//     console.log("clalling hello()");
+//     hello();
+//     console.log("clalling hello(), done");
+// }
+
+// console.log("calling demo");
+// demo();
+
+// console.log("done, tata bye!");
