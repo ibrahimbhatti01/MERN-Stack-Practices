3360978911eadaa48c05326fd7f70ea613e10ef4
Ibrahim <ib.8686299@gmail.com>
2025-10-18 22:12:06 +0500

    done with API response handling using axios

--- Backend Practices/Day 27 playing with API's , handling responses/app.js
+++ Backend Practices/Day 27 playing with API's , handling responses/app.js
@@ -1,9 +1,29 @@
-(async function(url){
+let CAT_API = "https://api.thecatapi.com/v1/images/search";
+
+
+let image = document.querySelector(".cat-image");
+
+let generateBtn = document.querySelector(".btn-generate");
+
+let imgPlaceholder = document.querySelector(".placeholder");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    imgPlaceholder.style.display = "none";
+    image.src = `${ await getRandCat(CAT_API) }`;
+})
+
+generateBtn.addEventListener("click", async () => {
+    imgPlaceholder.style.display = "none";
+    image.src = `${ await getRandCat(CAT_API) }`;
+})
+
+
+
+async function getRandCat(link){
     try {
-        let response = await axios.get(url); // return object with exact readable json data;
-        console.log(response.data[0].url);
+        let response = await axios.get(link); // return object with exact readable json data;
+        return response.data[0].url;
     } catch (error) {
         console.log(error);
     }
-    
-})(url)
+}
