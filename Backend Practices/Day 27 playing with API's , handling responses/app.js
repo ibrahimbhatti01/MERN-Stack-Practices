bd929e4d45ab3bd4e510bd5e9f04d53bf86a093b
Ibrahim <ib.8686299@gmail.com>
2025-10-20 16:29:28 +0500

    implemented cat imgaes app using another api

--- Backend Practices/Day 27 playing with API's , handling responses/app.js
+++ Backend Practices/Day 27 playing with API's , handling responses/app.js
@@ -1,5 +1,5 @@
 let CAT_API = "https://api.thecatapi.com/v1/images/search";
-
+// https://cataas.com/cat/orange,cute
 
 let image = document.querySelector(".cat-image");
 
@@ -27,6 +27,41 @@ async function getRandCat(link){
         console.log(error);
     }
 }
+
+
+
+
+// // v2 of cat images app, implemented using another api.
+
+// let CAT_API = "https://cataas.com/cat/orange,cute";
+// // https://api.thecatapi.com/v1/images/search
+
+// let image = document.querySelector(".cat-image");
+
+// let generateBtn = document.querySelector(".btn-generate");
+
+// let imgPlaceholder = document.querySelector(".placeholder");
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     imgPlaceholder.style.display = "none";
+//     image.src = `${ await getRandCat(CAT_API) }`;
+// })
+
+// generateBtn.addEventListener("click", async () => {
+//     imgPlaceholder.style.display = "none";
+//     image.src = `${ await getRandCat(CAT_API) }`;
+// })
+
+
+
+// async function getRandCat(link){
+//     try {
+//         let response = await axios.get(link); // return object with exact readable json data;
+//         return response.data.url;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 
 
